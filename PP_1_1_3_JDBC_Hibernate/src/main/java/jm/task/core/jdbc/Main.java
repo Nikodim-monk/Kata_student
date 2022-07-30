46e6b5aa55e6cb2666562695da8ac336ef22fabe
Nikodim-monk <lr1975@yandex.ru>
2022-07-30 16:36:07 +0400

    Add project

--- PP_1_1_3_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -7,15 +7,15 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 public class Main {
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDaoHibernateImpl();
-//        UserDao userDao = new UserDaoJDBCImpl();
+//        UserDao userDao = new UserDaoHibernateImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
 
         userDao.createUsersTable();
         userDao.saveUser("Иван", "Иванов", (byte) 20);
         userDao.saveUser("Сидор", "Сидоров", (byte) 30);
         userDao.saveUser("Петр", "Петров", (byte) 40);
         userDao.saveUser("Michael", "Jackson", (byte) 50);
-//        userDao.removeUserById(2);
+        userDao.removeUserById(2);
         userDao.getAllUsers();
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
--- PP_1_1_4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -21,30 +21,30 @@ public class Util {
         }
     }
 
-//    public static SessionFactory mineHiberConnection() {
-//        SessionFactory sessionFactory = null;
-//        try {
-//            Configuration configuration = new Configuration();
-//
-//            Properties settings = new Properties();
-//            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-//            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mine_schema");
-//            settings.put(Environment.USER, "root");
-//            settings.put(Environment.PASS, "Lroberon_75*Kata");
-//            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-//            settings.put(Environment.SHOW_SQL, "true");
-//            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//
-//            configuration.setProperties(settings);
-//            configuration.addAnnotatedClass(User.class);
-//
-//            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-//                    .applySettings(configuration.getProperties()).build();
-//
-//            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return sessionFactory;
-//    }
+    public static SessionFactory mineHiberConnection() {
+        SessionFactory sessionFactory = null;
+        try {
+            Configuration configuration = new Configuration();
+
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mine_schema");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "Lroberon_75*Kata");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
 }
